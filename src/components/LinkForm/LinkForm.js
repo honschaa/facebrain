a293b07ad720ce3e6d84a176ae873bda1dc816c4
honschaa <45900433+honschaa@users.noreply.github.com>
2020-07-17 11:15:48 +0800

    first commit

---
+++ src/components/LinkForm/LinkForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./LinkForm.css";
+
+const LinkForm = ({ onButtonSubmit, onInputChange }) => {
+  return (
+    <div>
+      <p className="f3">{"This magic brain will detect faces"}</p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input
+            className="f4 pa2  w-70 center"
+            type="text"
+            onChange={onInputChange}
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 div white bg-light-purple"
+            onClick={onButtonSubmit}
+          >
+            Detect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LinkForm;
